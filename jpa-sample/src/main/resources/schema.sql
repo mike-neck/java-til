895035dc93704006d9b90c8f073ddcf15af72eeb
mike_neck <jkrt3333@gmail.com>
2017-05-27 18:04:48 +0900

    テーブル増やした

--- jpa-sample/src/main/resources/schema.sql
+++ jpa-sample/src/main/resources/schema.sql
@@ -10,3 +10,26 @@ CREATE TABLE application_user
 create index application_user__index_username
   on application_user (username)
 ;
+
+
+CREATE TABLE email_address
+(
+    id BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    address VARCHAR(127) NOT NULL
+);
+CREATE UNIQUE INDEX email_address_address_uindex ON email_address (address);
+
+create table user_email
+(
+  user_id bigint not null,
+  email_id bigint not null,
+  created_at datetime not null,
+  primary key (user_id, email_id),
+  constraint user_email_application_user_id_fk
+  foreign key (user_id) references application_user (id),
+  constraint user_email_email_address_id_fk
+  foreign key (email_id) references email_address (id)
+);
+
+create index user_email_email_address_id_fk
+  on user_email (email_id);
