2bff6456643d97b7682860c5c8d7c26af2513931
mike_neck <jkrt3333@gmail.com>
2017-06-15 22:13:17 +0900

    delete all sql

---
+++ tree-model/docker/tree/db/deleta.sql
@@ -0,0 +1,14 @@
+DELETE FROM closure_table_project_user;
+DELETE FROM closure_table_project;
+DELETE FROM project_names;
+
+DELETE FROM nested_set_project_user;
+DELETE FROM nested_set_project;
+
+DELETE FROM path_enum_project_user;
+DELETE FROM path_enum_project;
+
+DELETE FROM employees;
+DELETE FROM users;
+DELETE FROM organizations;
+
