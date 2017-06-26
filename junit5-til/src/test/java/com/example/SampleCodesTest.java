4aae5f3b5f6be7b6d98d0147f3e9483de885e459
mike_neck <jkrt3333@gmail.com>
2017-06-27 00:19:28 +0900

    importをoptimizeした

--- junit5-til/src/test/java/com/example/SampleCodesTest.java
+++ junit5-til/src/test/java/com/example/SampleCodesTest.java
@@ -27,7 +27,6 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
