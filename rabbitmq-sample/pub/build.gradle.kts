0b316c09a386808f7eaba6dc845cefb0d7c7196c
mike_neck <jkrt3333@gmail.com>
2017-11-04 17:33:15 +0900

    rabbit-mq のチュートリアルプロジェクト

---
+++ rabbitmq-sample/pub/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java-library")
+    id("org.junit.platform.gradle.plugin")
+    kotlin("jvm") version("1.1.51")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    api("com.rabbitmq:amqp-client:5.0.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
+}
