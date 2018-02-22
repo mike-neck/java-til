189191733da9dc629ce8d0c3ce1937d02e7390b3
mike_neck <jkrt3333@gmail.com>
2018-02-23 02:04:17 +0900

    flatMapping の実装

--- reactor-tutorial/src/main/java/com/example/TransformSupplierImpl.java
+++ reactor-tutorial/src/main/java/com/example/TransformSupplierImpl.java
@@ -32,6 +32,6 @@ public class TransformSupplierImpl implements TransformSupplier {
 
     @Override
     public Flux<Name> flatMappingFlux(final Flux<User> flux) {
-        return null;
+        return flux.flatMap(User::asyncCapitalizedName);
     }
 }
