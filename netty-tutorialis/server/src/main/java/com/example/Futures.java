f881ab49e84aab780166a8bd91d1870bae77cfdb
mike_neck <jkrt3333@gmail.com>
2018-03-11 16:38:26 +0900

    タイムサーバーもどき

---
+++ netty-tutorialis/server/src/main/java/com/example/Futures.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 Shinya Mochida
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+@Slf4j
+public final class Futures {
+
+    private static final Futures instance = new Futures();
+
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    private Futures() {
+        Runtime.getRuntime().addShutdownHook(new Thread(executor::shutdown));
+    }
+
+    private <S> CompletableFuture<S> completableFuture(final Future<? extends S> future) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return future.get();
+            } catch (InterruptedException | ExecutionException e) {
+                log.warn("exception in executing task.", e);
+                throw new RuntimeException(e);
+            }
+        }, executor);
+    }
+
+    public static <S> CompletableFuture<S> toCompletableFuture(final Future<? extends S> future) {
+        return instance.completableFuture(future);
+    }
+}
---
+++ netty-tutorialis/time/src/main/java/com/example/time/TimeServerHandler.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2018 Shinya Mochida
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.time;
+
+import com.example.Futures;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Slf4j
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+    private final ZoneId zoneId = ZoneId.of("UTC");
+
+    @Override
+    public void channelActive(final ChannelHandlerContext ctx) {
+        log.info("channel active");
+        final ByteBuf byteBuf = ctx.alloc().buffer(8);
+        final LocalDateTime now = LocalDateTime.now(zoneId);
+        final long epochSecond = now.toInstant(ZoneOffset.UTC).getEpochSecond();
+        byteBuf.writeLong(epochSecond);
+        Mono.fromFuture(Futures.toCompletableFuture(ctx.writeAndFlush(byteBuf)))
+                .subscribe(v -> ctx.close());
+    }
+
+    @Override
+    public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) {
+        log.warn("exception caught on channelActive", cause);
+        ctx.close();
+    }
+
+    public static void main(String[] args) {
+        final long time = System.currentTimeMillis() / 1000 + 2208988800L;
+        System.out.println(time);
+        final Date date = new Date(time);
+        System.out.println(date);
+        System.out.println(date.toInstant().atOffset(ZoneOffset.UTC));
+    }
+}
