83aa5d4edcd141dd7175d6f8d71fd1ebf644d848
mike_neck <jkrt3333@gmail.com>
2018-03-11 15:59:58 +0900

    よくない共通化

---
+++ netty-tutorialis/server/src/main/java/com/example/GracefulShutdown.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2018 Shinya Mochida
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import lombok.RequiredArgsConstructor;
+
+public interface GracefulShutdown extends AutoCloseable {
+
+    @Override
+    default void close() {
+        gracefulShutdown();
+    }
+
+    void gracefulShutdown();
+
+    static GracefulShutdown gracefulShutdown(final AutoCloseable... resources) {
+        final int length = resources.length;
+        GracefulShutdown shutdown = NoOpShutdown.instance;
+        for (int i = length - 1; i >= 0; i--) {
+            shutdown = new Shutdown(resources[i], shutdown);
+        }
+        return shutdown;
+    }
+}
+
+final class NoOpShutdown implements GracefulShutdown {
+
+    static NoOpShutdown instance = new NoOpShutdown();
+
+    private NoOpShutdown() {}
+
+    @Override
+    public void gracefulShutdown() {
+    }
+}
+
+@RequiredArgsConstructor
+class Shutdown implements GracefulShutdown {
+
+    private final AutoCloseable closeable;
+    private final GracefulShutdown next;
+
+    @Override
+    public void gracefulShutdown() {
+        try {
+            closeable.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            next.gracefulShutdown();
+        }
+    }
+}
---
+++ netty-tutorialis/server/src/main/java/com/example/ServerMain.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 Shinya Mochida
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class ServerMain {
+
+    private final int port;
+
+    public void run() throws InterruptedException {
+        final NioEventLoopGroup parentGroup = new NioEventLoopGroup();
+        final NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try (final GracefulShutdown ignored =
+                     GracefulShutdown.gracefulShutdown(parentGroup::shutdownGracefully, workerGroup::shutdownGracefully)) {
+            final ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(parentGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(ChannelInitializationConfigurer.channelInitializer())
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            log.info("echo server start");
+            final ChannelFuture channelFuture = bootstrap.bind(port).sync();
+            channelFuture.channel().closeFuture().sync();
+        }
+    }
+}
