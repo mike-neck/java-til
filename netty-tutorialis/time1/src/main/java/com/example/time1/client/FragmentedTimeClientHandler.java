7bbb58bd609e78e1a1f0f80da798d37c956dd92e
mike_neck <jkrt3333@gmail.com>
2018-03-12 23:12:02 +0900

    チュートリアルの内容を写す

--- netty-tutorialis/time1/src/main/java/com/example/time1/client/FragmentedTimeClientHandler.java
+++ netty-tutorialis/time1/src/main/java/com/example/time1/client/FragmentedTimeClientHandler.java
@@ -31,23 +31,25 @@ public class FragmentedTimeClientHandler extends ChannelInboundHandlerAdapter {
 
     private ByteBuf byteBuf;
 
+    // ライフサイクル - handlerAdded
     @Override
-    public void handlerAdded(final ChannelHandlerContext ctx) throws Exception {
+    public void handlerAdded(final ChannelHandlerContext ctx) {
         this.byteBuf = ctx.alloc().buffer(4);
     }
 
+    // ライフサイクル - handlerRemoved
     @Override
-    public void handlerRemoved(final ChannelHandlerContext ctx) throws Exception {
+    public void handlerRemoved(final ChannelHandlerContext ctx) {
         byteBuf.release();
         byteBuf = null;
     }
 
     @Override
-    public void channelRead(final ChannelHandlerContext ctx, final Object msg) throws Exception {
+    public void channelRead(final ChannelHandlerContext ctx, final Object msg) {
         final ByteBuf buf = (ByteBuf) msg;
-        byteBuf.writeBytes(buf);
+        byteBuf.writeBytes(buf); // すべてのデータを byteBuf に移動
         buf.release();
-        if (byteBuf.readableBytes() >= 8) {
+        if (byteBuf.readableBytes() >= 8) { // データサイズの確認
             final long epochSeconds = byteBuf.readLong();
             final OffsetDateTime serverTime = Instant.ofEpochSecond(epochSeconds).atOffset(ZoneOffset.UTC);
             log.info("server time: {}", serverTime);
@@ -56,7 +58,7 @@ public class FragmentedTimeClientHandler extends ChannelInboundHandlerAdapter {
     }
 
     @Override
-    public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) throws Exception {
+    public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) {
         log.warn("exception occurred in channelRead", cause);
         ctx.close();
     }
