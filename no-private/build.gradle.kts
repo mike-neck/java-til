d504223c0679b8eb5034965573c6ce11d8b85627
mike_neck <jkrt3333@gmail.com>
2018-04-26 21:29:31 +0900

    reactor を追加

--- no-private/build.gradle.kts
+++ no-private/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 dependencies {
     api("org.eclipse.collections:eclipse-collections:9.0.0")
     implementation("org.projectlombok:lombok:1.16.18")
+    api (group = "io.projectreactor", name = "reactor-core", version = "3.1.6.RELEASE")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.0.1")
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-params", version = "5.0.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.0.1")
